bf180b07b4ce20a8f18152023f23137c905513a1
Jozef Wagner <jozef.wagner@gmail.com>
2013-07-19 12:18:22 +0200

    init

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject com.wagjo/benchmark-cljs "0.1.0-SNAPSHOT"
+  :description "Benchmarking ClojureScript stuff."
+  :url "https://github.com/wagjo/benchmark-cljs"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.2"] ;; needed for piggieback
+                 [com.wagjo/core.rrb-vector "0.1.0-SNAPSHOT"]
+                 [com.wagjo/clojurescript "0.1.0-SNAPSHOT"]
+                 [com.wagjo/data-cljs "0.1.0-SNAPSHOT"]
+                 [com.wagjo/diff-cljs "0.1.0-SNAPSHOT"]
+                 [com.wagjo/tools-cljs "0.1.0-SNAPSHOT"]
+                 [com.cemerick/piggieback "0.0.5"
+                   :exclusions [org.clojure/clojurescript
+                                org.clojure/clojure]]]
+  :source-paths ["src/clj" "src/cljs"]
+  :global-vars {*warn-on-reflection* true}
+  :repl-options {:port 41817
+                 :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as ClojureScript"})
